46a72f1abe5ffa32b2b3d8ecd534f718bed66d46
odhyp <pradhana.odhy@gmail.com>
2025-02-26 20:23:33 +0700

    feat: add playwright installer in install_packages

--- src/installer.py
+++ src/installer.py
@@ -2,9 +2,15 @@ import subprocess
 import sys
 
 
-def install_packages(packages: list):
+def install_packages(packages: list, install_playwright: bool = False):
     try:
         subprocess.check_call([sys.executable, "-m", "pip", "install", *packages])
+
+        if install_playwright:
+            subprocess.check_call(
+                [sys.executable, "-m", "playwright", "install", "chromium"]
+            )
+
     except subprocess.CalledProcessError as e:
         print(f"Failed to install packages: {e}")
         sys.exit(1)
