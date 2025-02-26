c8cc420d386965604a823be23c2d62b85a5a25a8
odhyp <pradhana.odhy@gmail.com>
2025-02-26 20:27:56 +0700

    refactor: move read_json to utils.py

--- src/update_site.py
+++ src/update_site.py
@@ -1,12 +1,7 @@
 from datetime import datetime
 from pathlib import Path
-import json
 
-
-def read_json(file_path: str) -> dict:
-    with open(file_path, "r", encoding="utf-8") as f:
-        data = json.load(f)
-    return data
+from src.utils import read_json
 
 
 def create_markdown(portfolio_data: dict):
