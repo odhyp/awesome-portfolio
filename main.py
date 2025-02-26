456453d50c99b184d69b8368fb7e11126e3c3321
odhyp <pradhana.odhy@gmail.com>
2025-02-26 20:27:17 +0700

    feat: update main.py with more args

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import argparse
 
 from src.installer import install_packages
-from src.utils import read_json
+from src import update_data, update_site, update_ss
 
 
 def main():
@@ -9,13 +9,25 @@ def main():
     parser.add_argument(
         "--update-data", action="store_true", help="Run the update_data function"
     )
+    parser.add_argument(
+        "--update-ss", action="store_true", help="Run the update_ss function"
+    )
+    parser.add_argument(
+        "--update-site", action="store_true", help="Run the update_site function"
+    )
 
     args = parser.parse_args()
 
     if args.update_data:
-        requirements = ["rich"]
-        install_packages(requirements)
-        print("Called function update data")
+        update_data.main()
+
+    if args.update_ss:
+        requirements = ["playwright"]
+        install_packages(requirements, install_playwright=True)
+        update_ss.main()
+
+    if args.update_site:
+        update_site.main()
 
 
 if __name__ == "__main__":
