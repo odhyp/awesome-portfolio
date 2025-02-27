0b3bb908a74b7879dbef5d9d2f607fe6e2985019
odhyp <pradhana.odhy@gmail.com>
2025-02-27 19:20:30 +0700

    feat: add latest and all parameter

--- src/update_ss.py
+++ src/update_ss.py
@@ -1,5 +1,7 @@
 from playwright.sync_api import sync_playwright
 
+from src.utils import read_json, ensure_folder_exists
+
 
 def take_screenshot(url: str, output_path: str):
     with sync_playwright() as p:
@@ -15,8 +17,34 @@ def take_screenshot(url: str, output_path: str):
             browser.close()
 
 
-def main():
-    url = "https://antfu.me/"
-    output_path = "screenshot.png"
+def main(update_type: str = "latest"):
+    output_dir = "site/static/ss"
+    ensure_folder_exists(output_dir)
 
-    take_screenshot(url, output_path)
+    portfolio_json = "data/portfolio.json"
+    portfolio_data = read_json(portfolio_json)
+
+    if update_type == "latest":
+        latest_data = portfolio_data[-1]
+
+        url = latest_data["liveUrl"]
+        ss_name = latest_data["liveName"]
+        output_path = f"{output_dir}/{ss_name}.png"
+
+        try:
+            take_screenshot(url, output_path)
+            print(f"Update Screenshot: {url} saved as {output_path}")
+        except Exception as e:
+            print(f"Update Screenshot failed: {e}")
+
+    if update_type == "all":
+        for data in portfolio_data:
+            url = data["liveUrl"]
+            ss_name = data["liveName"]
+            output_path = f"{output_dir}/{ss_name}.png"
+
+            try:
+                take_screenshot(url, output_path)
+                print(f"Update Screenshot: {url} saved as {output_path}")
+            except Exception as e:
+                print(f"Update Screenshot failed: {e}")
