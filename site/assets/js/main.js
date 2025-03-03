8e68fb0b2b30f6d3f6ac6eed9452254c80c92ba2
odhyp <pradhana.odhy@gmail.com>
2025-03-03 09:06:44 +0700

    feat(site): add header scroll effects

--- site/assets/js/main.js
+++ site/assets/js/main.js
@@ -1,1 +1,16 @@
-console.log("Hugo TailwindCSS Starter!");
+document.addEventListener("scroll", () => {
+  headerScrollEffect();
+});
+
+function headerScrollEffect() {
+  const scrollPoint = 150;
+  const pageHeader = document.getElementById("header");
+
+  if (window.scrollY > scrollPoint) {
+    pageHeader.classList.remove("bg-transparent");
+    pageHeader.classList.add("bg-neutral-100", "shadow-md");
+  } else {
+    pageHeader.classList.add("bg-transparent");
+    pageHeader.classList.remove("bg-neutral-100", "shadow-md");
+  }
+}
